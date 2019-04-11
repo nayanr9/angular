ff45abf75a87add9b1505bfa72f380038afc14f5
Raj Nayan <Ihcmhslciy~2008>
2019-04-11 18:36:24 +0530

    fixed the dom re-rendering issue.

--- src/app/Components/calculator/calculator.component.ts
+++ src/app/Components/calculator/calculator.component.ts
@@ -1,33 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-calculator',
-  templateUrl: './calculator.component.html',
-  styleUrls: ['./calculator.component.css']
+    selector: 'app-calculator',
+    templateUrl: './calculator.component.html',
+    styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent implements OnInit {
 
-  constructor() { }
-
-  firstValue : number = 0;
-  secondValue : number = 0;
-  resultValue : any = 0;
-  operand: string = '+';
-  error: string = 'Operator not found';
-
-  ngOnInit() {
-  }
-
-  process() {
-    switch (this.operand) {
-      case '+':
-        this.resultValue = this.firstValue + this.secondValue;
-        break;
-    
-      default:
-      this.resultValue = this.error;
-        break;
+    constructor() {
     }
-  }
+
+    firstValue: number = null;
+    secondValue: number = null;
+    resultValue: any = null;
+    operand = '-';
+    error = 'Operator not found';
+
+    ngOnInit() {
+    }
+
+    process() {
+        switch (this.operand) {
+            case '+':
+                this.resultValue = this.firstValue + this.secondValue;
+                break;
+            case '-':
+                this.resultValue = this.firstValue - this.secondValue;
+                break;
+            default:
+                this.resultValue = this.error;
+                break;
+        }
+    }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from "@angular/forms";
+import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SimpleComponentComponent } from './Components/simple-component/simple-component.component';
 import { CalculatorComponent } from './Components/calculator/calculator.component';
 
 const routes: Routes = [
-  { path: 'calculator', component: CalculatorComponent }
+    {path: 'calculator', component: CalculatorComponent}
 ];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SimpleComponentComponent,
-    CalculatorComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    RouterModule.forRoot(routes)
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        CalculatorComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        RouterModule.forRoot(routes)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
